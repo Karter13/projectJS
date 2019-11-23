29f4279a01ec17fa9169cd1620a9a9ecfc751b77
Karter13 <KarMiH1975@mail.ru>
2019-11-23 17:44:33 +0300

    Создал файлы index.html, script.js, .gitignore, .jshintrc, сделал ДЗ № 1

---
+++ script.js
@@ -0,0 +1,25 @@
+'use sctrict';
+
+let money = prompt('Ваш бюджет на месяц', ''),
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+  budjet: money,
+  timeData: prompt,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+let question1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+  question2 = prompt('Во сколько обойдется?', ''),
+  question3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+  question4 = prompt('Во сколько обойдется?', '');
+
+appData.expenses[question1] = question2;
+appData.expenses[question3] = question4;
+
+console.log(appData.budjet / 30);
+
+console.log(appData);
